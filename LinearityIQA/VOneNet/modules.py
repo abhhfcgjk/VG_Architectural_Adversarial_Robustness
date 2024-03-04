68d2f18016533844ad1065537a800be2f668ad72
abhhfcgjk <egorrr124@gmail.com>
2024-03-04 21:59:42 +0300

    vnnet

--- LinearityIQA/VOneNet/modules.py
+++ LinearityIQA/VOneNet/modules.py
@@ -111,8 +111,13 @@ class VOneBlock(nn.Module):
     def gabors_f(self, x):
         s_q0 = self.simple_conv_q0(x)
         s_q1 = self.simple_conv_q1(x)
-        c = self.complex(torch.sqrt(s_q0[:, self.simple_channels:, :, :] ** 2 +
-                                    s_q1[:, self.simple_channels:, :, :] ** 2) / np.sqrt(2))
+        # print(s_q0[:, self.simple_channels:, :, :])
+
+        v1 = torch.clamp(s_q0[:, self.simple_channels:, :, :].clone(),  min=10e-8) #+ torch.clamp(s_q0[:, self.simple_channels:, :, :].clone(),  max=-10e-2)
+        v2 = torch.clamp(s_q1[:, self.simple_channels:, :, :].clone(), min=10e-8) #+ torch.clamp(s_q1[:, self.simple_channels:, :, :].clone(), max=-10e-2)
+        val =  v1** 2 + v2 ** 2
+        
+        c = self.complex(torch.sqrt(val)/np.sqrt(2))
         s = self.simple(s_q0[:, 0:self.simple_channels, :, :])
         return self.gabors(self.k_exc * torch.cat((s, c), 1))
 
