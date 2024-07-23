2db93cf03828dfa970ec373c4ae15ee7a4d8a36e
28i_mel <28i_mel@vg-gpu-01.lab.graphicon.ru>
2024-07-23 16:07:50 +0300

    combinations results

--- t.sh
+++ t.sh
@@ -15,3 +15,15 @@ CUDA_VISIBLE_DEVICES=0 python main.py --dataset_path ./NIPS_test/ -arch resnet50
 CUDA_VISIBLE_DEVICES=0 python main.py --dataset_path ./NIPS_test/ -arch debiasedresnet50 --activation relu --device cuda --csv_results_dir rs -iter 1 -prune 0.1 -t_prune pls
 CUDA_VISIBLE_DEVICES=0 python main.py --dataset_path ./NIPS_test/ -arch debiasedresnet50 --activation relu --device cuda --csv_results_dir rs -iter 3 -prune 0.1 -t_prune pls
 CUDA_VISIBLE_DEVICES=0 python main.py --dataset_path ./NIPS_test/ -arch debiasedresnet50 --activation relu --device cuda --csv_results_dir rs -iter 5 -prune 0.1 -t_prune pls
+
+CUDA_VISIBLE_DEVICES=0 python main.py --dataset_path ./NIPS_test/ -arch debiasedresnet50 --activation elu --device cuda --csv_results_dir rs -iter 1 -prune 0.1 -t_prune pls
+CUDA_VISIBLE_DEVICES=0 python main.py --dataset_path ./NIPS_test/ -arch debiasedresnet50 --activation elu --device cuda --csv_results_dir rs -iter 3 -prune 0.1 -t_prune pls
+CUDA_VISIBLE_DEVICES=0 python main.py --dataset_path ./NIPS_test/ -arch debiasedresnet50 --activation elu --device cuda --csv_results_dir rs -iter 5 -prune 0.1 -t_prune pls
+
+CUDA_VISIBLE_DEVICES=0 python main.py --dataset_path ./NIPS_test/ -arch debiasedresnet50 --activation relu --device cuda --csv_results_dir rs -iter 1 -prune 0.1 -t_prune pls
+CUDA_VISIBLE_DEVICES=0 python main.py --dataset_path ./NIPS_test/ -arch debiasedresnet50 --activation relu --device cuda --csv_results_dir rs -iter 3 -prune 0.1 -t_prune pls
+CUDA_VISIBLE_DEVICES=0 python main.py --dataset_path ./NIPS_test/ -arch debiasedresnet50 --activation relu --device cuda --csv_results_dir rs -iter 5 -prune 0.1 -t_prune pls
+
+CUDA_VISIBLE_DEVICES=0 python main.py --dataset_path ./NIPS_test/ -arch debiasedresnet50 --activation elu --device cuda --csv_results_dir rs -iter 1
+CUDA_VISIBLE_DEVICES=0 python main.py --dataset_path ./NIPS_test/ -arch debiasedresnet50 --activation elu --device cuda --csv_results_dir rs -iter 3
+CUDA_VISIBLE_DEVICES=0 python main.py --dataset_path ./NIPS_test/ -arch debiasedresnet50 --activation elu --device cuda --csv_results_dir rs -iter 5
