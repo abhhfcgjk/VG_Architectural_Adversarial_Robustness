55ce76c3a81ad5c478fb74a5b2463c592b15a64b
Игорь Мелешин <28i_mel@vg-gpu-01.lab.graphicon.ru>
2024-12-02 11:31:28 +0300

    DBCNN with dali

---
+++ activ.py
@@ -0,0 +1,151 @@
+import scipy.special as sci
+from torch import nn
+import torch.nn.functional as F
+from torch import Tensor, exp, sqrt
+from torch.autograd import Function
+import numpy as np
+
+class Activaion_forward_ReLU_backward_SiLU(Function):
+    @staticmethod
+    def forward(ctx, x, inplace):
+        result = F.relu(x, inplace=inplace)
+        # x = x.cpu()
+        ex = exp(-x)
+        prod = x*ex
+        # x = x.cpu()
+        dx = (1 / (1 + ex) + prod / (1 +ex) ** 2) # d(x*sigmoid(x))/dx
+        
+        ctx.save_for_backward(dx)
+        ctx.inplace = inplace
+        return result
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        dx, = ctx.saved_tensors
+        # dx = dx.cuda()
+        result = grad_output * dx
+        inplace = ctx.inplace
+        return result, None
+    
+class Activaion_forward_ReLU_backward_ELU(Function):
+    @staticmethod
+    def forward(ctx, x, inplace):
+        result = F.relu(x, inplace=inplace)
+        # dx = 1 if x>=0 else exp(x) # d(ELU(x,alpha=1))/dx
+        dx = exp(x)
+        dx[dx >= 0] = 1
+        
+        ctx.save_for_backward(dx)
+        ctx.inplace = inplace
+        return result
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        dx, = ctx.saved_tensors
+        result = grad_output * dx
+        inplace = ctx.inplace
+        return result, None
+    
+class Activaion_forward_ReLU_backward_GELU(Function):
+    @staticmethod
+    def forward(ctx, x, inplace):
+        result = F.relu(x, inplace=inplace)
+        dx = 0.5 + 0.5*sci.erf(x.cpu()/np.sqrt(2)).cuda() + 0.5*x*(2/np.sqrt(np.pi)*exp(-x*x/2))
+        ctx.save_for_backward(dx)
+        ctx.inplace = inplace
+        return result
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        dx, = ctx.saved_tensors
+        result = grad_output * dx
+        inplace = ctx.inplace
+        return result, None
+
+
+class ReLU_SiLU(nn.Module):
+    """
+    Activation function is ReLU in forward.
+    Activation function is SiLU in backward.
+    """
+    __constants__ = ['inplace']
+    inplace: bool
+
+    def __init__(self, inplace: bool = False):
+        super(ReLU_SiLU, self).__init__()
+        self.inplace = inplace
+
+    def forward(self, input: Tensor) -> Tensor:
+        result = Activaion_forward_ReLU_backward_SiLU.apply(input, self.inplace)
+        return result
+
+    def extra_repr(self) -> str:
+        inplace_str = 'inplace=True' if self.inplace else ''
+        return inplace_str
+    
+class ReLU_ELU(nn.Module):
+    """
+    Activation function is ReLU in forward.
+    Activation function is SiLU in backward.
+    """
+    __constants__ = ['inplace']
+    inplace: bool
+
+    def __init__(self, inplace: bool = False):
+        super(ReLU_ELU, self).__init__()
+        self.inplace = inplace
+
+    def forward(self, input: Tensor) -> Tensor:
+        result = Activaion_forward_ReLU_backward_ELU.apply(input, self.inplace)
+        return result
+
+    def extra_repr(self) -> str:
+        inplace_str = 'inplace=True' if self.inplace else ''
+        return inplace_str
+    
+class ReLU_GELU(nn.Module):
+    """
+    Activation function is ReLU in forward.
+    Activation function is SiLU in backward.
+    """
+    __constants__ = ['inplace']
+    inplace: bool
+
+    def __init__(self, inplace: bool = False):
+        super(ReLU_GELU, self).__init__()
+        self.inplace = inplace
+
+    def forward(self, input: Tensor) -> Tensor:
+        result = Activaion_forward_ReLU_backward_GELU.apply(input, self.inplace)
+        return result
+
+    def extra_repr(self) -> str:
+        inplace_str = 'inplace=True' if self.inplace else ''
+        return inplace_str
+
+
+def ReLU_to_SILU(model):
+    """Swap ReLU activation to SiLU."""
+    for name, layer in model.named_children():
+        if isinstance(layer, nn.ReLU):
+            setattr(model, name, nn.SiLU())
+        else:
+            ReLU_to_SILU(layer)
+
+
+def ReLU_to_ReLUSiLU(model):
+    """Swap ReLU activation to ReLU_SiLU"""
+    for name, layer in model.named_children():
+        if isinstance(layer, nn.ReLU):
+            setattr(model, name, ReLU_SiLU())
+        else:
+            ReLU_to_ReLUSiLU(layer)
+
+
+def swap_all_activations(model, from_activation: nn.Module, to_activation: nn.Module):
+    for name, layer in model.named_children():
+        if isinstance(layer, from_activation):
+            setattr(model, name, to_activation())
+        else:
+            swap_all_activations(layer, from_activation, to_activation())
+
---
+++ model.py
@@ -0,0 +1,231 @@
+from collections import OrderedDict
+
+import torch
+import torch.nn as nn
+import torchvision
+from torchvision.ops import RoIPool, RoIAlign
+import numpy as np
+from torch.nn import ReLU, SiLU, ELU, GELU
+import torch.nn.functional as F
+
+from icecream import ic
+ic.disable()
+
+from Cayley import CayleyBlockPool
+# from activ import swap_all_activations, ReLU_ELU, ReLU_SiLU
+import activ
+
+def weight_init(net): 
+    for m in net.modules():    
+        if isinstance(m, nn.Conv2d):         
+            nn.init.kaiming_normal_(m.weight.data,nonlinearity='relu')
+            m.bias.data.zero_()
+        elif isinstance(m, nn.Linear):
+            nn.init.kaiming_normal_(m.weight.data,nonlinearity='relu')
+            m.bias.data.zero_()
+        elif isinstance(m, nn.BatchNorm2d):
+            m.weight.data.fill_(1)
+            m.bias.data.zero_()
+
+class SCNN(nn.Module):
+
+    def __init__(self):
+        """Declare all needed layers."""
+        super(SCNN, self).__init__()
+
+        # Linear classifier.
+        self.num_class = 39
+        self.features = nn.Sequential(nn.Conv2d(3,48,3,1,1),nn.BatchNorm2d(48),nn.ReLU(inplace=True),
+                                      nn.Conv2d(48,48,3,2,1),nn.BatchNorm2d(48),nn.ReLU(inplace=True),
+                                      nn.Conv2d(48,64,3,1,1),nn.BatchNorm2d(64),nn.ReLU(inplace=True),
+                                      nn.Conv2d(64,64,3,2,1),nn.BatchNorm2d(64),nn.ReLU(inplace=True),
+                                      nn.Conv2d(64,64,3,1,1),nn.BatchNorm2d(64),nn.ReLU(inplace=True),
+                                      nn.Conv2d(64,64,3,2,1),nn.BatchNorm2d(64),nn.ReLU(inplace=True),
+                                      nn.Conv2d(64,128,3,1,1),nn.BatchNorm2d(128),nn.ReLU(inplace=True),
+                                      nn.Conv2d(128,128,3,1,1),nn.BatchNorm2d(128),nn.ReLU(inplace=True),
+                                      nn.Conv2d(128,128,3,2,1),nn.BatchNorm2d(128),nn.ReLU(inplace=True))
+        weight_init(self.features)
+        self.pooling = nn.AvgPool2d(14,1)
+        self.projection = nn.Sequential(nn.Conv2d(128,256,1,1,0), nn.BatchNorm2d(256), nn.ReLU(inplace=True),
+                                        nn.Conv2d(256,256,1,1,0), nn.BatchNorm2d(256), nn.ReLU(inplace=True))
+        weight_init(self.projection)    
+        self.classifier = nn.Linear(256,self.num_class)
+        weight_init(self.classifier)
+
+    def forward(self, X):
+#        return X
+        N = X.size()[0]
+        assert X.size() == (N, 3, 224, 224)
+        X = self.features(X)
+        assert X.size() == (N, 128, 14, 14)
+        X = self.pooling(X)
+        assert X.size() == (N, 128, 1, 1)
+        X = self.projection(X)
+        X = X.view(X.size(0), -1)          
+        X = self.classifier(X)
+        assert X.size() == (N, self.num_class)
+        return X
+
+
+class DBCNN(torch.nn.Module):
+
+    def __init__(self, scnn_root, **options):
+        """Declare all needed layers."""
+        nn.Module.__init__(self)
+
+        # Convolution and pooling layers of VGG-16.
+        self.features1 = torchvision.models.vgg16(pretrained=True).features
+        self.features1 = nn.Sequential(*list(self.features1.children())[:-1])
+        
+        if options.get('cayley', None) or options.get('cayley2', None):
+            self.cayley = CayleyBlockPool(in_channels=512, intermed_channels=200)
+            self.features1 = nn.Sequential(*list(self.features1.children())[:-2], 
+                                            self.cayley, 
+                                            *list(self.features1.children())[-2:])
+        elif options.get('cayley3', None) or options.get('cayley4', None):
+            self.cayley = CayleyBlockPool(in_channels=512, intermed_channels=200)
+            self.features1 = nn.Sequential(*list(self.features1.children())[:-6], 
+                                            self.cayley, 
+                                            *list(self.features1.children())[-6:])
+
+        scnn = SCNN()
+        scnn = torch.nn.DataParallel(scnn).cuda()
+              
+        scnn.load_state_dict(torch.load(scnn_root))
+        self.features2 = scnn.module.features
+        if options.get('cayley2',None):
+            self.cayley2 = CayleyBlockPool(in_channels=128, intermed_channels=100)
+            self.features2 = nn.Sequential(*list(self.features2.children())[:-3], 
+                                            self.cayley2, 
+                                            *list(self.features2.children())[-3:])
+        elif options.get('cayley4',None):
+            self.cayley4 = CayleyBlockPool(in_channels=64, intermed_channels=64)
+            self.features2 = nn.Sequential(*list(self.features2.children())[:-9], 
+                                            self.cayley4, 
+                                            *list(self.features2.children())[-9:])
+        
+        # Linear classifier.
+        self.fc = torch.nn.Linear(512*128, 1)
+        
+        self.__set_activation(options.get('activation', None))
+
+        if options['fc'] == True:
+            # Freeze all previous layers.
+            for param in self.features1.parameters():
+                param.requires_grad = False
+            for param in self.features2.parameters():
+                param.requires_grad = False
+            # Initialize the fc layers.
+            nn.init.kaiming_normal_(self.fc.weight.data)
+            if self.fc.bias is not None:
+                nn.init.constant_(self.fc.bias.data, val=0)
+
+    def __set_activation(self, activ_function):
+        if activ_function=='Frelu_elu':
+            activ.swap_all_activations(self.features1, nn.ReLU, activ.ReLU_ELU)
+            # activ.swap_all_activations(self.features2, nn.ReLU, activ.ReLU_ELU)
+            self.Activ = activ.ReLU_ELU
+        elif activ_function=='Frelu_silu':
+            activ.swap_all_activations(self.features1, nn.ReLU, activ.ReLU_SiLU)
+            # activ.swap_all_activations(self.features2, nn.ReLU, activ.ReLU_SiLU)
+            self.Activ = activ.ReLU_SiLU
+        elif activ_function=='Felu':
+            activ.swap_all_activations(self.features1, nn.ReLU, nn.ELU)
+            # activ.swap_all_activations(self.features2, nn.ReLU, nn.ELU)
+            self.Activ = nn.ELU
+        elif activ_function=='Fsilu':
+            activ.swap_all_activations(self.features1, nn.ReLU, nn.SiLU)
+            # activ.swap_all_activations(self.features2, nn.ReLU, nn.SiLU)
+            self.Activ = nn.SiLU
+        elif activ_function=='Fgelu':
+            activ.swap_all_activations(self.features1, nn.ReLU, nn.GELU)
+            # activ.swap_all_activations(self.features2, nn.ReLU, nn.GELU)
+            self.Activ = nn.GELU
+        else:
+            activ.swap_all_activations(self.features1, nn.ReLU, nn.ReLU)
+            self.Activ = nn.ReLU
+
+    def _euclidian_mapping(self, B):
+        #ic(torch.sqrt(torch.abs(B)))
+        B_mul = torch.sign(B)*torch.sqrt(torch.abs(B))
+        #ic(B_mul)
+        
+        mapped_B = B_mul / (torch.norm(B_mul) + 1e-8)
+        return mapped_B
+
+    def forward(self, X):
+        """Forward pass of the network."""
+        N = X.size()[0]
+
+        ic(X)
+        X1 = self.features1(X)
+
+        x = X
+        for name, module in self.features1.named_children():
+            x = module(x)
+            ic(module)
+            ic(x + 1e-8)
+
+        ic(X1.shape)
+        # ic(X1)
+
+        H = X1.size()[2]
+        W = X1.size()[3]
+        
+        assert X1.size()[1] == 512
+        X2 = self.features2(X)
+        ic(X2.shape)
+        H2 = X2.size()[2]
+        W2 = X2.size()[3]
+        ic(X2)
+        assert X2.size()[1] == 128        
+        
+        if (H != H2) | (W != W2):
+            X2 = F.upsample_bilinear(X2,(H,W))
+
+        X1 = X1.view(N, 512, H*W)
+        X2 = X2.view(N, 128, H*W)  
+        # ic(X1)
+        # X = torch.bmm(X1, torch.transpose(X2, 1, 2)) / (H*W)  # Bilinear
+        X = torch.bmm(X1, torch.transpose(X2, 1, 2))
+        assert X.size() == (N, 512, 128)
+        X = X.view(N, 512*128)
+        # X = torch.sqrt(X + 1e-8) # WARN: Nan values after sqrt
+        # X = torch.nn.functional.normalize(X)
+        #ic(X)
+        X = self._euclidian_mapping(X + 1e-8)
+        #ic(X)
+        X = self.fc(X)
+        assert X.size() == (N, 1)
+        return X
+
+
+class ImageNormalizer(nn.Module):
+    def __init__(
+        self,
+        mean: tuple[float, float, float] = [0.485, 0.456, 0.406],
+        std: tuple[float, float, float] = [0.229, 0.224, 0.225],
+        persistent: bool = True,
+    ):
+        super(ImageNormalizer, self).__init__()
+
+        self.register_buffer(
+            "mean", torch.as_tensor(mean).view(1, 3, 1, 1), persistent=persistent
+        )
+        self.register_buffer(
+            "std", torch.as_tensor(std).view(1, 3, 1, 1), persistent=persistent
+        )
+
+    def forward(self, inputs: torch.Tensor):
+        return (inputs - self.mean) / self.std
+
+
+def normalize_model(
+    model: nn.Module,
+    mean: tuple[float, float, float] = [0.485, 0.456, 0.406],
+    std: tuple[float, float, float] = [0.229, 0.224, 0.225],
+):
+    # layers = OrderedDict([("crop", tv.transforms.Resize([691, 921])), ("normalize", ImageNormalizer(mean, std)), ("model", model)])
+    layers = OrderedDict([("normalize", ImageNormalizer(mean, std)), ("model", model)])
+    return nn.Sequential(layers)
+
